4d86945da417ca1c5f22b077f31b12377717e1c0
Dianajs89 <151823415+Dianajs89@users.noreply.github.com>
2023-11-26 19:38:27 +0100

    Add files via upload

---
+++ Argumentos_2/src/main/java/com/mycompany/argumentos_2/Argumentos_2.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.argumentos_2;
+
+
+public class Argumentos_2 {
+
+    public static void main(String[] args) {
+        
+        String argumento0 = args [0];
+        String argumento1 = args [1];
+        
+        int argumento1_numero =Integer.parseInt(argumento1);
+        
+        int num =1;
+        
+        
+        for(int i=0;i<argumento1_numero;i++){
+            System.out.println("producto " +num+":" + argumento0 +" " + i);
+                 
+            num++;
+            
+        }
+        
+    }
+}
